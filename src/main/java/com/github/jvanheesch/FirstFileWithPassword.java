3139601b38b1acfec043f4102216c9ad3ea088fe
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-12-06 12:33:38 +0100

    generated

--- src/main/java/com/github/jvanheesch/FirstFileWithPassword.java
+++ src/main/java/com/github/jvanheesch/FirstFileWithPassword.java
@@ -2,4 +2,6 @@ package com.github.jvanheesch;
 
 public class FirstFileWithPassword {
     private static final String PASSWORD = "nobodyCaresAboutSecurity";
+    private static final String KEY4 = "16245500513";
+    private static final String KEY5 = "18161437187";
 }
